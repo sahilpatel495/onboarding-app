578be2e31731ac22d852b0ca5c0a865f65717049
sahilpatel495 <sahilpatel495@gmail.com>
2022-09-20 23:26:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Dashboard from './container/Dashboard';
 import "./App.css"
+import { useLogify } from 'react-logify';
 
 function App() {
+   const { user, onLogin, onLogout } = useLogify();
+
   return (
     <div className="App">
-      <Dashboard/>
+      <>
+        {!user.id && (
+          <center style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            heigth: "100vh",
+          }}>
+            <button onClick={onLogin}>Signup / Login</button>
+          </center>
+        )}
+        {user.id && (
+          <>
+            <center>
+              {user.name}
+              
+              <button onClick={onLogout}>Logout</button>
+            </center>
+            <Dashboard />
+          </>
+        )}
+      </>
     </div>
   );
 }
